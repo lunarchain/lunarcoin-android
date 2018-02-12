3993726bf5a58dc1097f6554ae94070b744cf3c5
qikh <kaihua.qi@gmail.com>
2018-02-13 00:05:23 +0800

    Initial commit.

---
+++ app/src/main/java/io/lunarchain/MainActivity.kt
@@ -0,0 +1,52 @@
+package io.lunarchain
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_wallet -> {
+                currentFragment = WalletFragment()
+                val ft = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.content, currentFragment)
+                ft.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_miner -> {
+                currentFragment = MinerFragment()
+                val ft = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.content, currentFragment)
+                ft.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_transfer -> {
+                currentFragment = TransferFragment()
+                val ft = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.content, currentFragment)
+                ft.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private lateinit var currentFragment: Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        currentFragment = WalletFragment()
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.content, currentFragment)
+        ft.commit()
+
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
